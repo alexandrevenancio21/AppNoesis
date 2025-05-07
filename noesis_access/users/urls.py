1a235bb17cc7bd97992699b96e16552bbccdc29e
Alexandre Venâncio <alexandredsvenancio@gmail.com>
2025-05-08 00:03:40 +0100

    third commit

--- noesis_access/users/urls.py
+++ noesis_access/users/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 from .views import CustomLoginView, homepage
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('homepage/', homepage, name='homepage'),
+    path('ativos/clientList/', TemplateView.as_view(template_name='ativos/clientList.html'), name='client_list'),
+    path('ativos/pii/', TemplateView.as_view(template_name='ativos/pii.html'), name='pii'),
+    path('ativos/testingOn/', TemplateView.as_view(template_name='ativos/testingOn.html'), name='testing_on'),
+    path('ativos/customers/', TemplateView.as_view(template_name='ativos/customers.html'), name='customers'),
+    path('ativos/vqm/', TemplateView.as_view(template_name='ativos/vqm.html'), name='vqm'),
+    path('ativos/suppliers/', TemplateView.as_view(template_name='ativos/suppliers.html'), name='suppliers'),
+    path('ativos/lowCode/', TemplateView.as_view(template_name='ativos/lowCode.html'), name='low_code'),
+    path('ativos/manuals/', TemplateView.as_view(template_name='ativos/manuals.html'), name='manuals'),
 ]
